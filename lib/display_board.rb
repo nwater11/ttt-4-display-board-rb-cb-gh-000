15b43642cb34a42875a90d4db20cf41a201df67a
Jane Smith <5904002440@rumail.ru.ac.th>
2017-09-14 01:53:30 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,3 +1,3 @@
 def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} \n-----------\n #{board[3]} | #{board[4]} | #{board[5]} |\n-----------\n #{board[6]} | #{board[7]} | #{board[8]} |"
+  puts " #{board[0]} | #{board[1]} | #{board[2]} \n-----------\n #{board[3]} | #{board[4]} | #{board[5]} \n-----------\n #{board[6]} | #{board[7]} | #{board[8]} "
 end
